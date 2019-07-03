3ae8a37cde6bf58cd1c275fdbd2506d21090cf33
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-07-03 11:24:15 +0200

    test | test sonarqube analysis

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,3 +1,3 @@
 mvn sonar:sonar \
->   -Dsonar.projectKey=com.eraldo.sudoku-validator \
->   -Dsonar.host.url=http://173.249.41.180:9000 \
+   -Dsonar.projectKey=com.eraldo.sudoku-validator \
+   -Dsonar.host.url=http://173.249.41.180:9000 \
